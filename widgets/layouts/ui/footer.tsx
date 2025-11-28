c585edcf04664dc4a98bfe514b79d77a28cdb9b2
박창조 <ckdwh1202@gmail.com>
2025-11-28 16:32:09 +0900

    feat: add header, footer component

---
+++ widgets/layouts/ui/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-4">
+      <p className="text-sm text-muted-foreground">
+        copyright 2025. changjo Park. All rights reserved.
+      </p>
+    </footer>
+  );
+}
---
+++ widgets/layouts/ui/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="bg-background py-4">
+      <h1 className="text-2xl font-bold">기술 블로그 썸네일 생성기</h1>
+      <p className="text-base text-muted-foreground mt-2">
+        주제, 관련 태그, 분위기에 알맞는 썸네일 이미지를 생성해줍니다.
+      </p>
+    </header>
+  );
+}
